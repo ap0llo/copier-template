95426722668df1942e5a7db2258e6e09fd6122bd
Andreas Grünwald <andreas@grynwald.net>
2024-05-02 20:37:01 +0200

    Add initial version of template supporting both .NET library and Cake Addin projects

---
+++ build/workflow/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Cake.AzurePipelines.Module;
+using Cake.Core;
+using Cake.DotNetLocalTools.Module;
+using Cake.Frosting;
+using Grynwald.SharedBuild;
+
+return new CakeHost()
+    .UseModule<AzurePipelinesModule>()
+    .UseModule<LocalToolsModule>()
+    .InstallToolsFromManifest(".config/dotnet-tools.json")
+    .UseSharedBuild<BuildContext>()
+    .Run(args);
+
+
+public class BuildContext : DefaultBuildContext
+{
+    public override IReadOnlyCollection<IPushTarget> PushTargets { get; } = new IPushTarget[]
+    {
+        // TODO
+        // new PushTarget(
+        //     type: PushTargetType.AzureArtifacts,
+        //     feedUrl: "https://pkgs.dev.azure.com/ap0llo/OSS/_packaging/PublicCI/nuget/v3/index.json",
+        //     isActive: context => context.Git.IsMainBranch || context.Git.IsReleaseBranch
+        // ),
+        // new PushTarget(
+        //     PushTargetType.NuGetOrg,
+        //     "https://api.nuget.org/v3/index.json",
+        //     context => context.Git.IsReleaseBranch
+        // )
+    };
+
+
+    public BuildContext(ICakeContext context) : base(context)
+    { }
+}
